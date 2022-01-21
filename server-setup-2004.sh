28f25b2c5aa27d6e2c7af2f3d6e32affa2973e6f
saurabhroy <saurabh@benlycos.com>
2022-01-21 17:07:26 +0530

    Inculed support package for server

--- server-setup-2004.sh
+++ server-setup-2004.sh
@@ -5,8 +5,13 @@
 #-----------------------------------------------------------------------------------------------#
 
 sudo apt-get update
-<<<<<<< HEAD
 sudo apt-get -y install python-pip
-=======
 sudo pip install speedtest-cli 
->>>>>>> 0705412dc93cc5a62ccb7c2e41547e8a9bcfce1f
+sudo apt-get -y install monit
+sudo apt-get -y install trash-cli
+sudo apt-get -y install fail2ban
+sudo apt-get -y install python-apt
+sudo apt-get -y install htop
+sudo apt-get -y install iftop
+sudo apt-get -y install bwm-ng
+sudo apt-get install s3cmd
